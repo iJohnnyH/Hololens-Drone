65636bce218321cd143377633b74d9fad9743136
Johnny <ijohnnyh@gmail.com>
2017-02-24 14:28:27 -0500

    Added 5 millsecond delay per messege

--- Hololens-Drone/Assets/TCPClient.cs
+++ Hololens-Drone/Assets/TCPClient.cs
@@ -24,40 +24,58 @@ public class TCPClient : MonoBehaviour {
 
 	}
 
-    void main() {
+    void main() { 
         try
         {
             client = new TcpClient();
             Debug.Log("Connecting.....");
 
-            client.Connect("152.23.202.92", 5717);
-            // use the ipaddress as in the server program
+            client.Connect("152.23.1.224", 5717);
+            //Use RPi ip address
 
             Debug.Log("Connected");
 
-            String str = "some commands";
-            String str2 = "test1";
-            String str3 = "test3";
+            String str = "1";
+            String str2 = "2";
+            String str3 = "3";
+            String str4 = "4";
+            String str5 = "5";
             String quitstr = "QUIT";
             Stream stm = client.GetStream();
 
             ASCIIEncoding asen = new ASCIIEncoding();
 
-            byte[] ba = asen.GetBytes(str);
-            Debug.Log("Transmitting.....");
+            byte[] strData = asen.GetBytes(str);
+            Debug.Log("Writing 1");
+            stm.Write(strData, 0, strData.Length);
+            System.Threading.Thread.Sleep(5);
 
-            stm.Write(ba, 0, ba.Length);
+            Debug.Log("Writing 2");
+            strData = asen.GetBytes(str2);
+            stm.Write(strData, 0, strData.Length);
+            System.Threading.Thread.Sleep(5);
 
-            ba = asen.GetBytes(str2);
-            stm.Write(ba, 0, ba.Length);
+            Debug.Log("Writing 3");
+            strData = asen.GetBytes(str3);
+            stm.Write(strData, 0, strData.Length);
+            System.Threading.Thread.Sleep(5);
 
-            ba = asen.GetBytes(str3);
-            stm.Write(ba, 0, ba.Length);
+            Debug.Log("Writing 4");
+            strData = asen.GetBytes(str4);
+            stm.Write(strData, 0, strData.Length);
+            System.Threading.Thread.Sleep(5);
 
-            ba = asen.GetBytes(quitstr);
-            stm.Write(ba, 0, ba.Length);
+            Debug.Log("writing 5");
+            strData = asen.GetBytes(str5);
+            stm.Write(strData, 0, strData.Length);
+            System.Threading.Thread.Sleep(5);
 
-            Debug.Log("Messege sent, closing client");
+            Debug.Log("Writing QUIT");
+            strData = asen.GetBytes(quitstr);
+            stm.Write(strData, 0, strData.Length);
+            System.Threading.Thread.Sleep(5);
+
+            Debug.Log("Messege(s) sent, closing client");
             client.Close();
         }
         catch (Exception e)

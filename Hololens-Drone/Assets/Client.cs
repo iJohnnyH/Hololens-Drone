580fe0c3976e5f883b98a2b90c210afa92e3e95c
Johnny <ijohnnyh@gmail.com>
2017-03-31 16:13:42 -0400

    Added handshaking protocol and sending random camera movements

--- Hololens-Drone/Assets/Client.cs
+++ Hololens-Drone/Assets/Client.cs
@@ -18,24 +18,31 @@ public class Client : MonoBehaviour
 #endif
 #if WINDOWS_UWP
     public StreamSocket socket;
+    String avail = "TRUE";
     // Use this for initialization
     async void Start()
     {
         Debug.Log("---STARTING CLIENT---");
-        await connect("152.23.42.40", "5717");
+        await connect("152.23.111.43", "5717");
         Debug.Log("---SENDING MESSEGES---");
+
+        /*await sendCameraRotation(-300, -600, -1500);
+        await waitAvail();
+        await sendCameraRotation(0, 0, 0);
+        await waitAvail();
+        await sendCameraRotation(300, 200, 1700);
+        await waitAvail();*/
         while (true)
         {
-            //sendCameraRotation();
-            Debug.Log(transform.eulerAngles.x + " " + transform.eulerAngles.y + " " + transform.eulerAngles.z);
-            Task.Delay(5000);
+            await sendRandomCameraAngles();
+            await waitAvail();
         }
         await send("QUIT");
         socket.Dispose();
         Debug.Log("---ENDING CLIENT---");
     }
 
-    async public void sendCameraRotation()
+    async public Task sendCameraRotation()
     {
         /* await send("s");
          Task.Delay(5);
@@ -47,9 +54,22 @@ public class Client : MonoBehaviour
          Task.Delay(5);*/
     }
 
+    async public Task sendCameraRotation(float x, float y, float z)
+    {
+        await send("s");
+        await Task.Delay(10);
+        await send(x.ToString());
+        await Task.Delay(10);
+        await send(y.ToString());
+        await Task.Delay(10);
+        await send(z.ToString());
+        await Task.Delay(10);
+        avail = "";
+    }
+
     public async Task connect(string host, string port)
     {
-        Debug.Log("---ATTEMPTING TO CONNECT TO SERVER");
+        Debug.Log("---ATTEMPTING TO CONNECT TO SERVER---");
         HostName hostName;
         socket = new StreamSocket();
         hostName = new HostName(host);
@@ -115,6 +135,40 @@ public class Client : MonoBehaviour
         // In order to prolong the lifetime of the stream, detach it from the DataWriter
         writer.DetachStream();
         writer.Dispose();
+    }
+
+    public async Task recieve()
+    {
+        DataReader reader = new DataReader(socket.InputStream);
+        reader.UnicodeEncoding = Windows.Storage.Streams.UnicodeEncoding.Utf8;
+        reader.ByteOrder = Windows.Storage.Streams.ByteOrder.LittleEndian;
+        reader.InputStreamOptions = InputStreamOptions.Partial;
+        var count = await reader.LoadAsync(4);
+        avail = reader.ReadString(count);
+        Debug.Log(avail);
+    }
+
+    public async Task waitAvail()
+    {
+        while (avail != "TRUE")
+        {
+            await recieve();
+        }
+    }
+
+    public async Task sendRandomCameraAngles()
+    {
+        System.Random seed = new System.Random();
+        float roll = (float)seed.Next(-350, 351), pitch = (float)seed.Next(-900, 301), yaw = (float)seed.Next(-3200, 3201);
+        await send("s");
+        await Task.Delay(10);
+        await send(roll.ToString());
+        await Task.Delay(5);
+        await send(pitch.ToString());
+        await Task.Delay(5);
+        await send(yaw.ToString());
+        await Task.Delay(5);
+        avail = "";
     }
 #endif
 }
